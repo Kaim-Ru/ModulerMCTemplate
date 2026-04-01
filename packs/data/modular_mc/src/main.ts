cc9d2ab5e349692ac8a83de123ae1aca3d48fd72
Kaimaru <kuyama2018@gmail.com>
2026-04-01 20:04:16 +0900

    import方式の変更

--- packs/data/modular_mc/src/main.ts
+++ packs/data/modular_mc/src/main.ts
@@ -1,5 +1,5 @@
 import mc from "@minecraft/server";
-import { test } from "%SRC%/module.ts";
+import { test } from "@src/module.ts";
 
 mc.world.afterEvents.worldLoad.subscribe(() => {
   console.log("Hello, Modular MC!");
